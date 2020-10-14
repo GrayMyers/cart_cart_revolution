f6ef41542329ea58129c2f3dabfd01027de928f8
Grayson Myers <graymyers10@gmail.com>
2020-10-14 13:36:58 -0600

    Add attr_reader and products attribute

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,8 +1,10 @@
 require './lib/product.rb' #Could not load file unless it contained the extension .rb
 
 class ShoppingCart
+  attr_reader :name, :capacity, :products
   def initialize(name,capacity_string)
     @name = name
     @capacity = capacity_string.to_i
+    @products = []
   end
 end
