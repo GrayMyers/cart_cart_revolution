35080498e4709837f4785a928eaebfa9d7b7d40d
Grayson Myers <graymyers10@gmail.com>
2020-10-14 13:35:48 -0600

    Add tests for shopping cart class attributes

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -5,10 +5,26 @@ require './lib/shopping_cart.rb' #Same issue as above
 class ShoppingCartTest < Minitest::Test
 
   def setup
-    @shopping_cart = ShoppingCart.new("King Soopers","30items")
+    @shopping_cart_1 = ShoppingCart.new("King Soopers","30items")
+    @shopping_cart_2 = ShoppingCart.new("Safeway","20items")
+
   end
 
   def test_it_exists
-    assert_instance_of ShoppingCart, @shopping_cart
+    assert_instance_of ShoppingCart, @shopping_cart_1
+  end
+
+  def test_it_has_name
+    assert_equal "King Soopers",@shopping_cart_1.name
+    assert_equal "Safeway",@shopping_cart_2.name
+  end
+
+  def test_it_has_capacity
+    assert_equal 30,@shopping_cart_1.capacity
+    assert_equal 20,@shopping_cart_2.capacity
+  end
+
+  def test_it_has_products
+    assert_equal [], @shopping_cart_1.products
   end
 end
