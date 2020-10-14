c49203c8d3de1c6e8bceefa9b75db3d6b93ab6a2
Grayson Myers <graymyers10@gmail.com>
2020-10-14 13:16:17 -0600

    Add attr_reader and quantity method which converts instance variable (string) to integer

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,15 @@
 
 class Product
+attr_reader :category, :name, :unit_price
+
   def initialize(category,name,unit_price,quantity)
     @category = category
     @name = name
     @quantity = quantity
     @unit_price = unit_price
+  end
+
+  def quantity
+    @quantity.to_i
   end
 end
