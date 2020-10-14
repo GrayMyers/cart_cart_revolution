0f6c7612b904cfc37e14e0191b8badef11c6ccf2
Grayson Myers <graymyers10@gmail.com>
2020-10-14 13:24:24 -0600

    Add hoarded instance var, is_hoarded? and hoard methods to product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,16 +1,25 @@
 
 class Product
-attr_reader :category, :name, :unit_price
+attr_reader :category, :name, :unit_price, :hoarded
 
   def initialize(category,name,unit_price,quantity)
     @category = category
     @name = name
     @quantity = quantity
     @unit_price = unit_price
+    @hoarded = false
   end
 
-  def quantity
+  def quantity #IP shows instance variable quantity stored as string
     @quantity.to_i
+  end
+
+  def hoard
+    @hoarded = true
+  end
+
+  def is_hoarded? #IP uses question mark notation to output variable
+    @hoarded
   end
 
   def total_price
